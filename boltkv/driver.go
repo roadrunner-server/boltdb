72c95438dc27b044a8d2039654942848449e309a
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-03-12 09:24:33 +0100

    deps: update dependencies

--- boltkv/driver.go
+++ boltkv/driver.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/roadrunner-server/api/v2/plugins/config"
-	kvv1 "github.com/roadrunner-server/api/v2/proto/kv/v1beta"
+	kvv1 "github.com/roadrunner-server/api/v2/proto/kv/v1"
 	"github.com/roadrunner-server/errors"
 	"github.com/roadrunner-server/sdk/v2/utils"
 	bolt "go.etcd.io/bbolt"
